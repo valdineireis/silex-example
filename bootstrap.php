5e92f213c1723347b878486584d19de9a6372199
Valdinei Reis <valdineimtz@gmail.com>
2016-05-10 22:51:14 -0300

    instalação do twig

--- bootstrap.php
+++ bootstrap.php
@@ -4,3 +4,7 @@ require_once 'vendor/autoload.php';
 
 $app = new \Silex\Application();
 $app['debug'] = true;
+
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views',
+));
