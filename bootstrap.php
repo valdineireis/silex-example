d17e5c8b0bf1dd77342513c17fee596f156a7561
Valdinei Reis <valdineimtz@gmail.com>
2016-05-13 23:01:43 -0300

    adicionando a senha de acesso ao banco de dados

--- bootstrap.php
+++ bootstrap.php
@@ -47,7 +47,7 @@ $em = EntityManager::create(
         'host'     => 'localhost',
         'port'     => '3306',
         'user'     => 'root',
-        'password' => '',
+        'password' => 'root',
         'dbname'   => 'exemplo_doctrine',
     ),
     $config,
